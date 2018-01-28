04cdcd1189e810ab29894f60ddf202f8465af5f8
Matthew Casey <mdcasey@chabloom.com>
2018-01-28 03:53:30 -0500

    Update vkGetPhysicalDeviceSurfaceCapabilitiesKHR

--- src/vkGetPhysicalDeviceSurfaceCapabilitiesKHR.cpp
+++ src/vkGetPhysicalDeviceSurfaceCapabilitiesKHR.cpp
@@ -19,5 +19,28 @@ VKAPI_ATTR VkResult VKAPI_CALL vkGetPhysicalDeviceSurfaceCapabilitiesKHR(
     VkSurfaceKHR              surface,
     VkSurfaceCapabilitiesKHR* pSurfaceCapabilities)
 {
+    VkSurfaceCapabilitiesKHR surfaceCapabilities = {};
+    surfaceCapabilities.minImageCount            = 1;
+    surfaceCapabilities.maxImageCount            = 0;
+    surfaceCapabilities.minImageExtent.width     = 1;
+    surfaceCapabilities.minImageExtent.height    = 1;
+    surfaceCapabilities.maxImageArrayLayers      = 1;
+    surfaceCapabilities.supportedCompositeAlpha  = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR | VK_COMPOSITE_ALPHA_PRE_MULTIPLIED_BIT_KHR | VK_COMPOSITE_ALPHA_POST_MULTIPLIED_BIT_KHR;
+    surfaceCapabilities.supportedUsageFlags      = VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
+
+#if WINAPI_FAMILY == WINAPI_FAMILY_DESKTOP_APP
+    RECT windowRect = {};
+    GetWindowRect(surface->hwnd, &windowRect);
+
+    surfaceCapabilities.currentExtent.width   = windowRect.right - windowRect.left;
+    surfaceCapabilities.currentExtent.height  = windowRect.bottom - windowRect.top;
+    surfaceCapabilities.maxImageExtent.width  = GetSystemMetrics(SM_CXSCREEN);
+    surfaceCapabilities.maxImageExtent.height = GetSystemMetrics(SM_CYSCREEN);
+    surfaceCapabilities.supportedTransforms   = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+    surfaceCapabilities.currentTransform      = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+#endif
+
+    *pSurfaceCapabilities = surfaceCapabilities;
+
     return VK_SUCCESS;
 }
